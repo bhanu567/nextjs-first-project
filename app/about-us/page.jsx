d8effdb67687bffb677b3a05e350691792e8d329
bhanu567 <bhanupjha5465@gmail.com>
2024-07-08 22:33:56 +0530

    Addded Linking Property

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import "./page.scss";
+
 export default function AboutUs() {
-  return <h1>About Us</h1>;
+  return (
+    <div className="link-div">
+      <Link href="/about-us/1" className="lnk">
+        Yash
+      </Link>
+      <Link href="/about-us/2" className="lnk">
+        Vaibhav
+      </Link>
+      <Link href="/about-us/3" className="lnk">
+        Suresh
+      </Link>
+    </div>
+  );
 }
